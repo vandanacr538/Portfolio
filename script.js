fc138216066ed1649eb8dbab2361decaa902b632
vandanacr538 <vandanacr538@gmail.com>
2023-06-05 15:46:02 +0530

    Fixed: Scrolling issue when the icons present in haeder are clicked

--- script.js
+++ script.js
@@ -10,39 +10,23 @@ window.onload=function(){
             }
             items[i].className="items active";
             if(items[i].id==2){
-                document.getElementById("edu").scrollIntoView({
-                    behavior: "smooth", 
-                    block: "end", 
-                    inline: "nearest",
-                });
+                // const yOffset=-70;
+                // const element = document.getElementById("edu");
+                // const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                // window.scrollTo({top: y, behavior: 'smooth'});
+                document.getElementById("edu").scrollIntoView({ behavior: 'smooth', block: 'start'});
             }
             if(items[i].id==3){
-                document.getElementById("wrk").scrollIntoView({
-                    behavior: "smooth", 
-                    block: "end", 
-                    inline: "nearest",
-                });
+                document.getElementById("wrk").scrollIntoView({ behavior: 'smooth', block: 'start'});
             }
             if(items[i].id==4){
-                document.getElementById("prtfol").scrollIntoView({
-                    behavior: "smooth", 
-                    block: "end", 
-                    inline: "nearest",
-                });
+                document.getElementById("prtfol").scrollIntoView({ behavior: 'smooth', block: 'start'});
             }
             if(items[i].id==5){
-                document.getElementById("rcm").scrollIntoView({
-                    behavior: "smooth", 
-                    block: "end", 
-                    inline: "nearest",
-                });
+                document.getElementById("rcm").scrollIntoView({ behavior: 'smooth', block: 'start'});
             }
             if(items[i].id==6){
-                document.getElementById("cont").scrollIntoView({
-                    behavior: "smooth", 
-                    block: "end", 
-                    inline: "nearest",
-                });
+                document.getElementById("cont").scrollIntoView({ behavior: 'smooth', block: 'start'});
             }
             if(items[i].id==1){
                 window.scroll({
